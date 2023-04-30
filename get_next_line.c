7714469ec604a3936e78c6d973d7e3bb4d451906
Mustafa YAĞIZ <112881823+myagjz@users.noreply.github.com>
2023-04-30 23:16:01 +0300

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myagiz <myagiz@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/26 14:37:36 by myagiz            #+#    #+#             */
+/*   Updated: 2022/11/28 16:46:12 by myagiz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"get_next_line.h"
+
+char	*ft_read_to_left_str(int fd, char *left_str)
+{
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+		left_str = ft_strjoin(left_str, buff);
+	}
+	free(buff);
+	return (left_str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*left_str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	left_str = ft_read_to_left_str(fd, left_str);
+	if (!left_str)
+		return (NULL);
+	line = ft_get_line(left_str);
+	left_str = ft_new_left_str(left_str);
+	return (line);
+}
+
+/*
+int	main()
+{
+	char *str;
+	int i = open("a", O_RDONLY);
+	int number = 0;
+	while(number <= 1)
+	{
+		str = get_next_line(i);
+		printf("%s",str);
+		number++;
+	}
+}
+*/
